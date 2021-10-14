ce9ad88b5101e07e516814bfbb1e9685f62c80c5
Manuel Naval <navalrod@gmail.com>
2021-10-14 08:50:32 +0200

    cambio en tamaño de botones

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 export interface PeriodicElement {
+  id: String;
   nombre: string;
   apellidos: string;
   edad: number;
@@ -8,12 +9,12 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
-  {nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
-  {nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
-  {nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
-  {nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
-  {nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'}
+  {id: '1',nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
+  {id: '2',nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
+  {id: '3',nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
+  {id: '4',nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
+  {id: '5',nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
+  {id: '6',nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'}
 
 ];
 
@@ -26,6 +27,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: 'tabla.component.html',
 })
 export class TablaComponent {
-  displayedColumns: string[] = ['nombre', 'apellidos', 'edad', 'curso','edit','delete'];
+  displayedColumns: string[] = ['id','nombre', 'apellidos', 'edad', 'curso','edit','delete'];
   dataSource = ELEMENT_DATA;
 }
