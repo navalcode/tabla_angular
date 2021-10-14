18cf2c9b9cdcbd1667ae3bd1677d3c2c58cb6b88
Manuel Naval <navalrod@gmail.com>
2021-10-14 23:42:35 +0200

    añadidos cambios

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -1,6 +1,6 @@
 import {Component, NgModule} from '@angular/core';
 
-export interface PeriodicElement {
+export interface alumno {
   id: String;
   nombre: string;
   apellidos: string;
@@ -9,13 +9,13 @@ export interface PeriodicElement {
 }
 
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: alumno[] = [
   {id: '1',nombre: 'Pedro', apellidos: 'Gomara Lopez', edad: 22, curso: '2º DAM'},
   {id: '2',nombre: 'Jose', apellidos: 'Ortega y Gassete', edad: 21, curso: '2º DAM'},
   {id: '3',nombre: 'Ainara', apellidos: 'De los rios vazquez', edad: 22, curso: '2º DAM'},
   {id: '4',nombre: 'Mateo', apellidos: 'Gillen Gillen', edad: 25, curso: '2º DAM'},
   {id: '5',nombre: 'Alejandro', apellidos: 'Castro Polanco', edad: 21, curso: '2º DAM'},
-  {id: '6',nombre: 'Julia', apellidos: 'San Jose Khamer', edad: 21, curso: '2º DAM'}
+  {id: '6',nombre: 'Julia', apellidos: 'Vazquez Gons', edad: 21, curso: '2º DAM'}
 
 ];
 
@@ -33,12 +33,16 @@ export class TablaComponent {
   displayedColumns: string[] = ['id','nombre', 'apellidos', 'edad', 'curso','edit','delete'];
   dataSource = ELEMENT_DATA;
 
-  id:boolean = true;
-  nombre:boolean= true;
-  apellidos:boolean= true;
-  curso:boolean= true;
-  edad:boolean= true;
-  editar:boolean= true;
-  delete:boolean= true;
+  idChecked:boolean = true;
+  nombreChecked:boolean= true;
+  apellidosChecked:boolean= true;
+  cursoChecked:boolean= true;
+  edadChecked:boolean= true;
+  editarChecked:boolean= true;
+  deleteChecked:boolean= true;
+
+
+
+
 
 }
